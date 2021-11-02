747ac3069fd5b9f4f8dfa9ac35eabbe4cf9d5726
Ishtiaq Mahmood Amin <ishtiaq.mahmood@protonmail.com>
2021-11-02 22:36:05 +0100

    actions defs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,53 @@
 import * as ActionTypes from './action_types';
 
 export const getChannels = channels => ({
-    type: ActionTypes.GET_LIST_OF_RSS_CHANNELS,
-    payloag: channels
+  type: ActionTypes.GET_LIST_OF_RSS_CHANNELS,
+  payloag: channels,
 });
 
+
+// Channel -> Entries
 export const fetchEntries = channelUrl => ({
-    type: ActionTypes.GET_LIST_OF_ENTRIES,
-    payload: channelUrl
+  type: ActionTypes.GET_LIST_OF_ENTRIES,
+  payload: channelUrl,
 });
 
-export const fetchContents = feedContent => ({
-    type: ActionTypes.SHOW_ENTRY_CONTENTS,
-    payload: feedContent
+export const onFetchEntriesSucceeded = entries => ({
+  type: ActionTypes.GET_LIST_OF_ENTRIES_SUCCEEDED,
+  payload: entries,
+});
+
+export const onFetchEntriesFailed = error => ({
+  type: ActionTypes.GET_LIST_OF_ENTRIES_FAILED,
+  payload: error,
+});
+
+
+// Entries -> Content
+export const fetchContents = entryUrl => ({
+  type: ActionTypes.SHOW_ENTRY_CONTENTS,
+  payload: entryUrl,
+});
+
+export const onFetchContentsSucceeded = feedContent => ({
+  type: ActionTypes.SHOW_ENTRY_CONTENTS_SUCCEEDED,
+  payload: feedContent,
+});
+
+export const onFetchContentsFailed = error => ({
+  type: ActionTypes.SHOW_ENTRY_CONTENTS_FAILED,
+  payload: error,
+});
+
+
+// Entries -> Favorite
+export const markAsFavorite = (url, uniqueId) => ({
+    type: ActionTypes.MARK_AS_FAVORITE,
+    payload: {url: url, id: uniqueId}
+});
+
+// Show Favorite list
+export const showFavorite = (list=[]) => ({
+    type: ActionTypes.SHOW_LIST_OF_FAVORITE,
+    payload: list
 });
