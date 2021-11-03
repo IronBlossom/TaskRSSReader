0e3ca954bdcca4fb89242053915682b1256562ad
Ishtiaq Mahmood Amin <ishtiaq.mahmood@protonmail.com>
2021-11-03 02:02:13 +0100

    initialized channels data in reducers

--- src/reducers/channel_reducers.js
+++ src/reducers/channel_reducers.js
@@ -3,14 +3,24 @@ import {
   GET_LIST_OF_RSS_CHANNELS_FAILED,
 } from '../actions/action_types';
 const initialState = {
-  channels: null,
+  channels: [
+    {
+      name: 'CBN-Finance',
+      url: 'https://www1.cbn.com/rss-cbn-news-finance.xml',
+    },
+    {name: 'CBN-Health', url: 'https://www1.cbn.com/rss-cbn-news-health.xml'},
+    {
+      name: 'CBN-Politics',
+      url: 'https://www1.cbn.com/rss-cbn-news-politics.xml',
+    },
+  ],
   error: null,
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_LIST_OF_RSS_CHANNELS_SUCCEEDED:
-      return {...state, channels: action.payload};
+      return {...state};
     case GET_LIST_OF_RSS_CHANNELS_FAILED:
       return {...state, error: action.payload};
     default:
