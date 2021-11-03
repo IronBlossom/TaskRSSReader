b2a8cb2b46d8ca09949d14773824e739b043c592
Ishtiaq Mahmood Amin <ishtiaq.mahmood@protonmail.com>
2021-11-03 02:01:35 +0100

    fixed reducer encapsulation

--- src/reducers/channel_reducers.js
+++ src/reducers/channel_reducers.js
@@ -1,19 +1,19 @@
 import {
-    GET_LIST_OF_RSS_CHANNELS_SUCCEEDED,
-    GET_LIST_OF_RSS_CHANNELS_FAILED
-  
-  } from '../actions/action_types'
-  const initialState = {
-    channels: null,
-  };
-  
-  export default const channelReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_LIST_OF_RSS_CHANNELS_SUCCEEDED:
-          return{...state,action.payload};
-        case GET_LIST_OF_RSS_CHANNELS_FAILED:
-          return{...state,action.payload};
-      default:
-        return state;
-    }
-  };
+  GET_LIST_OF_RSS_CHANNELS_SUCCEEDED,
+  GET_LIST_OF_RSS_CHANNELS_FAILED,
+} from '../actions/action_types';
+const initialState = {
+  channels: null,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_LIST_OF_RSS_CHANNELS_SUCCEEDED:
+      return {...state, channels: action.payload};
+    case GET_LIST_OF_RSS_CHANNELS_FAILED:
+      return {...state, error: action.payload};
+    default:
+      return state;
+  }
+};
--- src/reducers/content_reducer.js
+++ src/reducers/content_reducer.js
@@ -1,20 +1,21 @@
 import {
-    SHOW_ENTRY_CONTENTS_SUCCEEDED,
-    SHOW_ENTRY_CONTENTS_FAILED
-  
-  } from '../actions/action_types'
-  const initialState = {
-    entries: null,
-    favorites: null,
-  };
-  
-  export default const contentReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SHOW_ENTRY_CONTENTS_SUCCEEDED:
-          return{...state,action.payload};
-        case SHOW_ENTRY_CONTENTS_FAILED:
-          return{...state,action.payload};
-      default:
-        return state;
-    }
-  };
+  SHOW_ENTRY_CONTENTS_SUCCEEDED,
+  SHOW_ENTRY_CONTENTS_FAILED,
+} from '../actions/action_types';
+const initialState = {
+  contents: null,
+  favorites: null,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SHOW_ENTRY_CONTENTS_SUCCEEDED:
+      return {...state, contents: action.payload};
+    case SHOW_ENTRY_CONTENTS_FAILED:
+      return {...state, error: action.payload};
+    default:
+      return state;
+  }
+};
+
